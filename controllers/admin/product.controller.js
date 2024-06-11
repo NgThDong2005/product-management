b13dfeaf4d77859ff0ce043a2bfa995f6ac34005
rangsut <143336737+rangsut@users.noreply.github.com>
2024-06-11 20:51:50 +0700

    Lọc theo trạng thái - backend

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -2,10 +2,16 @@ const Product = require("../../models/product.model");
 
 //[GET]  /admin/product/
 module.exports.index = async (req, res) => {
-    const products = await Product.find({
+    const find = {
         deleted: false
-    });
+    };
 
+    if(req.query.status) {
+        find.status = req.query.status;
+    }
+
+    const products = await Product.find(find);
+    
     console.log(products);
 
     res.render("admin/pages/products/index", {
