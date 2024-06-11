8009cf7bccb0561304ff6529cc3a77b0fb47d6dc
rangsut <143336737+rangsut@users.noreply.github.com>
2024-06-11 20:34:59 +0700

    Lấy data sản phẩm in ra giao diện

--- config/system.js
+++ config/system.js
@@ -1,3 +1,3 @@
 module.exports = {
-    prefixAdmin: "admin111"
+    prefixAdmin: "admin"
 };
--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,7 +1,17 @@
+const Product = require("../../models/product.model");
+
 //[GET]  /admin/product/
-module.exports.index = (req, res) => {
-    res.render("./admin/pages/products/index", {
-        pageTitle: "Quản lý sản phẩm"
+module.exports.index = async (req, res) => {
+    const products = await Product.find({
+        deleted: false
+    });
+
+    console.log(products);
+
+    res.render("admin/pages/products/index", {
+        pageTitle: "Quản lý sản phẩm",
+        products: products
     });
 }
+
 
