865d043e56114c004c1304ab8519f4c6ff502734
rangsut <143336737+rangsut@users.noreply.github.com>
2024-06-12 22:17:27 +0700

    Tối ưu phần bộ lọc

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -6,6 +6,21 @@ module.exports.index = async (req, res) => {
         deleted: false
     };
 
+    const filterStatus = [
+        {
+          label: "Tất cả",
+          value: ""
+        },
+        {
+          label: "Hoạt động",
+          value: "active"
+        },
+        {
+          label: "Dừng hoạt động",
+          value: "inactive"
+        },
+    ];
+    
     if(req.query.status) {
         find.status = req.query.status;
     }
@@ -20,12 +35,12 @@ module.exports.index = async (req, res) => {
 
     const products = await Product.find(find);
     
-    console.log(products);
 
     res.render("admin/pages/products/index", {
         pageTitle: "Quản lý sản phẩm",
         products: products,
-        keyword: keyword
+        keyword: keyword,
+        filterStatus: filterStatus
     });
 }
 
