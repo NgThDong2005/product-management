2f0fdc2680a38d3c5c1b641d067bad0907b0d065
rangsut <143336737+rangsut@users.noreply.github.com>
2024-06-16 13:53:28 +0700

    Tối ưu phần xóa

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -164,10 +164,8 @@ const listButtonDelete = document.querySelectorAll("[button-delete]");
 if(listButtonDelete.length > 0) {
   listButtonDelete.forEach(button => {
     button.addEventListener("click", () => {
-      const id = button.getAttribute("button-delete");
-      console.log(id);
-
-      fetch(`/admin/products/delete/${id}`, {
+      const link = button.getAttribute("button-delete");
+      fetch(link, {
         method: "PATCH"
       })
         .then(res => res.json())
